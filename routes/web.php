dc55aec358011a51fb023055d162dfce03372456
LeoHuang <leohuang1996@gmail.com>
2017-11-28 02:26:30 +0800

    建立home路由

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
+Route::get('/home', function () {
+     return view('home');
+ });
