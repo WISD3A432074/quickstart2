fa16ccab2ff552baa70f9e4183b1c7ac49aaaf25
LeoHuang <leohuang1996@gmail.com>
2017-11-28 02:25:59 +0800

    控制器的動作指派auth中介層

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,18 @@
  namespace App\Http\Controllers;
 
  use Illuminate\Http\Request;
+ use App\Http\Requests;
+ use App\Http\Controllers\Controller;
 
  class TaskController extends Controller
  {
-     //
+       /**
+         * 建立一個新的控制器實例。
+         *
+         * @return void
+         */
+        public function __construct()
+        {
+            $this->middleware('auth');
+        }
  }
